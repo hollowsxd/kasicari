5afbd66ba220db6a9454da5bdafb9f14b2dab98f
HollowsxD <hollowsxd@users.noreply.github.com>
2025-01-11 23:05:27 +0800

    Update middleware.js again

--- middleware.js
+++ middleware.js
@@ -1,12 +1,5 @@
 import { NextResponse } from 'next/server';
 
-// Middleware configuration
-export const config = {
-  matcher: [
-    '/((?!api|favicon.ico|login.html).*)', // Apply middleware to all paths except API, static files, and login.html
-  ],
-};
-
 export default function middleware(req) {
   const url = req.nextUrl.clone();
   const isLoggedIn = req.cookies.get('loggedIn') === 'true';
