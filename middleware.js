ecaf8334806f3df6fee35ec7698cda223933e8e1
HollowsxD <hollowsxd@users.noreply.github.com>
2025-01-12 11:02:58 +0800

    Update middleware.js matcher

--- middleware.js
+++ middleware.js
@@ -1,13 +1,12 @@
 import { NextResponse } from 'next/server';
 
+export const config = {
+  matcher: ['/((?!api|favicon.ico).*)'],
+};
+
 export default function middleware(req) {
   const url = req.nextUrl.clone();
   const isLoggedIn = req.cookies.get('loggedIn') === 'true';
-
-  // Exclude API routes and favicon.ico from middleware logic
-  if (pathname.startsWith('/api') || pathname === '/favicon.ico') {
-    return NextResponse.next();
-  }
 
   if (!isLoggedIn && url.pathname !== '/login.html') {
     url.pathname = '/login.html';
