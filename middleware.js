cd0e9a34d8b5a6bbb228bf3d124818f4b5949944
HollowsxD <hollowsxd@users.noreply.github.com>
2025-01-12 21:54:01 +0800

    add http response 302

--- middleware.js
+++ middleware.js
@@ -12,14 +12,14 @@ export default function middleware(req) {
   if (!isLoggedIn && pathname !== '/login.html') {
     const loginUrl = req.nextUrl.clone();
     loginUrl.pathname = '/login.html';
-    return NextResponse.rewrite(loginUrl); // Temporary redirect to login page
+    return NextResponse.redirect(loginUrl); // Temporary redirect to login page
   }
 
   // Redirect authenticated users away from the login page to the index
   if (isLoggedIn && pathname === '/login.html') {
     const indexUrl = req.nextUrl.clone();
     indexUrl.pathname = '/index.html';
-    return NextResponse.rewrite(indexUrl); // Redirect to index.html
+    return NextResponse.redirect(indexUrl, 302); // Redirect to index.html
   }
 
   return NextResponse.next();
